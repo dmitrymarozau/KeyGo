322bc4ea9f6aa29a49b99391009a5d6cc5360bf2
dmitrymarozau <dmitry.marozau@affirm.com>
2024-10-23 22:04:06 +0200

    Add gradle dependancies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -144,8 +144,10 @@ dependencies {
     androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.6.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("com.google.android.apps.common.testing.accessibility.framework:accessibility-test-framework:4.1.1")
 }
 
 room {
